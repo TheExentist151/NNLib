8b65710f16360a66388fcd2e99cece2f374ccd8b
Sergei Oganesov <91889396+TheExentist151@users.noreply.github.com>
2024-01-27 12:41:22 +0600

    Add values to the platforms

--- Source/NNLib/Common/Platforms.cs
+++ Source/NNLib/Common/Platforms.cs
@@ -1,6 +1,7 @@
 ﻿namespace NNLib.Common
 {
     // TODO: make this as an interface or smth
+    // TODO: maybe the values should be in hex? 
 
     /// <summary>
     /// Supported platforms in NN library
@@ -10,51 +11,51 @@
         /// <summary>
         /// Xbox 360 / Original, little endian, 16 alignment
         /// </summary>
-        NX,
+        NX = 22606,
 
         /// <summary>
         /// PlayStation 2, little endian, 16 (?) alignment
         /// </summary>
-        NS,
+        NS = 21326,
 
         /// <summary>
         /// Gamecube / Wii, big endian, 32 alignment
         /// </summary>
-        NG,
+        NG = 18254,
 
         /// <summary>
         /// PlayStation 3, little (?) endian, 16 (?) alignment
         /// </summary>
-        NC,
+        NC = 17230,
 
         /// <summary>
         /// PSP, little endian, 16 (?) alignment
         /// </summary>
-        NU,
+        NU = 21838,
 
         /// <summary>
         /// Nintendo DS / 3DS, little endian, 16 (?) alignment
         /// </summary>
-        DS,
+        DS = 21316,
 
         /// <summary>
         /// iOS / Android / Windows Phone, little endian, 16 (?) alignment
         /// </summary>
-        NI,
+        NI = 18766,
 
         /// <summary>
         /// Xbox 360 (wtf), little (?) endian, 16 (?) alignment
         /// </summary>
-        NE,
+        NE = 17742,
 
         /// <summary>
         /// Xbox 360 / PlayStation 3 (again? wtf?), little (?) endian, 16 (?) alignment
         /// </summary>
-        NY,
+        NY = 22862,
 
         /// <summary>
         /// Other platforms, little endian, 16 alignment
         /// </summary>
-        NZ
+        NZ = 23118
     }
 }
