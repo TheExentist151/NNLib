ffa4cd6c6df6a6cd0a24eb483fcc8420b41ff11e
Sergei Oganesov <91889396+TheExentist151@users.noreply.github.com>
2024-01-31 18:44:49 +0600

    Change how we skip unsupported chunks

--- Source/NNLib/File.cs
+++ Source/NNLib/File.cs
@@ -57,7 +57,7 @@ namespace NNLib
                                 Texlist.Read(reader, InfoChunk.FirstChunkOffset);
                                 break;
 
-                            default: reader.ReadBytes((int)header.ChunkHeader.NextChunkOffset - 16); break; // Skipping unsupported chunk
+                            default: reader.BaseStream.Seek(header.ChunkHeader.NextChunkOffset - 8, SeekOrigin.Current); break; // Skipping unsupported chunk
                         }
 
                         // Skipping padding until the 'N' letter is found
